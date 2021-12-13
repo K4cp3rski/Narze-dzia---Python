43a643309eb90a9ec426c5eaed8da8e68c25e1a9
K4cp3rskii <k.cybinski@student.uw.edu.pl>
2021-12-13 18:54:53 +0100

    Zadanie domowe z 13.12 v.1.1

--- cw_6.12/code/nbp_course_change/test_nbp.py
+++ cw_6.12/code/nbp_course_change/test_nbp.py
@@ -2,18 +2,6 @@ import pytest
 
 import nbp_change as nbp
 
-import requests
-
-# custom class to be the mock return value
-# will override the requests.Response returned from requests.get
-
-
-class MockResponse:
-    # mock json() method always returns a specific testing dictionary
-    @staticmethod
-    def json():
-        return {"mock_key": "mock_response"}
-
 
 def mocklist(currency_code, days):
     return [4.6834, 4.6494, 4.6002, 4.5934, 4.5889, 4.5991, 4.5941, 4.6226, 4.6129, 4.6204], 'euro'
@@ -22,7 +10,7 @@ def mocklist(currency_code, days):
 def test_calc_statistics(monkeypatch):
 
     # Podmieniamy w pliku zaimportowanym jako nbp funkcję get_courses na funkcję mocklist
-    # Funkcja mocklist zwraca stałą tabelę z danymi skopiowanymi z
+    # Funkcja mocklist zwraca stałą tabelę z danymi skopiowanymi z outputu funkcji "get_courses" dla 10 dni
     monkeypatch.setattr(nbp, "get_courses", mocklist)
 
     result = nbp.calc_statistics(['EUR'], 10)
